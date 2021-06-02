b4a1ff14c3b44c5e8d447b43b6b219b22a4fcfaa
akhr1997 <ak.hr1997@gmail.com>
2021-06-03 03:55:05 +0530

    Create Category Model, Serialize data from DB, Category API ROuting and Views

---
+++ ecom/api/category/serializers.py
@@ -0,0 +1,8 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Category
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'description')
